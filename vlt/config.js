b315f29ee2ff0eec33a9243f517190689b3133d2
Tiêu Kiếm <107684026+realtieukiem123@users.noreply.github.com>
2025-02-14 15:39:43 +0700

    up

---
+++ vlt/config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    introTitle: 'Em yêu à!',
+    introDesc: `Giữa cuộc đời trăm ngàn cám giỗ
+    Anh chỉ cần bến đỗ nơi em.`,
+    btnIntro: 'Dạ',
+    title: 'Soái ca thuộc về ngôn tình, Còn anh thì chỉ thuộc về em thôi 😙',
+    desc: 'Chỉ yêu mình em',
+    btnYes: 'Mãi yêu anh <33',
+    btnNo: 'Mơ đi cưng',
+    question:'Cho anh xin một lý do em iêu anh',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Bên nhau trọn đời em nhé',
+    btnAccept: 'Oki ạ <3',
+    messLink: 'https://www.imgamedev.com'
+}
