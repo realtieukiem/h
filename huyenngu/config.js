8b3ce7ba12849d066369f671ee83ca7e17baa5a6
Tiêu Kiếm <107684026+realtieukiem123@users.noreply.github.com>
2025-02-14 11:43:39 +0700

    y

---
+++ huyenngu/config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    introTitle: 'Gà à!',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao mày cứ đi nhầm đường
+    Lạc vào tim tao lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'hihi',
+    title: 'Phải chăng mày đã ngu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng mày đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Ok thích tao <33',
+    btnNo: 'Đéo :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao mày thích tao <3',
+    btnReply: 'Gửi cho bố mày <3',
+    reply: 'Yêu thì yêu mà không yêu thì phải yêu <33333333',
+    mess: 'Bố biết mà 🥰. Yêu mày nhiều nhiều 😘😘',
+    messDesc: 'Khỏi ốm bố m cho m đi chơi.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://m.me/phamanhtnvn' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
